e725fc48de4bcab909d0aad0e26f278b433c1ba0
vigneshpamu <vigneshpamu2002@gmail.com>
2025-03-22 02:55:12 +0530

    sd

--- Aryan_Final_Year/AI-Mock-Interview-App-with-Next-JS/app/page.js
+++ Aryan_Final_Year/AI-Mock-Interview-App-with-Next-JS/app/page.js
@@ -125,7 +125,7 @@ const HeroSection = () => {
 
             <div className="px-8 sm:items-center sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
               <a
-                href="/sign-in"
+                href="/dashboard"
                 title=""
                 className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                 role="button"
